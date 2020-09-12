e92f35c8ef1835bc8f7457bc0e661b7bab60b2eb
Marvin Xu <marvinxu99@hotmail.com>
2020-09-11 18:20:18 -0700

    working(polls removed)

--- main_project/settings.py
+++ main_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = False
 DOMAIN = config('DOMAIN', default='DEV')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.14']
