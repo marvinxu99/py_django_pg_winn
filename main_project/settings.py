97a2512931c4beb01865b5fac1e29f90975e3108
Marvin Xu <marvinxu99@hotmail.com>
2020-09-11 20:02:15 -0700

    ...

--- main_project/settings.py
+++ main_project/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'winn_la_1',
+        'NAME': 'winn_la_1', 
         'USER': 'winter',
         'PASSWORD': 'winter',
         'HOST': 'localhost',
