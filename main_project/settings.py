2e4fe2bb4f5e4641f5841998a6fe4774014dad0a
Marvin Xu <marvinxu99@hotmail.com>
2021-01-10 17:39:58 -0800

    updating settings.py to include prod deploy settings

--- main_project/settings.py
+++ main_project/settings.py
@@ -14,11 +14,14 @@ import os
 from decouple import config, Csv
 # import django_heroku
 from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse_lazy
 
+# Define custom user model
 AUTH_USER_MODEL = 'accounts.User'
-LOGIN_URL = 'accounts:login'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+
+LOGIN_URL = reverse_lazy('accounts:login')
+LOGIN_REDIRECT_URL = reverse_lazy('home')
+LOGOUT_REDIRECT_URL = reverse_lazy('home')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
