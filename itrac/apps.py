5369721a8edb556cc61c25741b223e304c9758f4
Marvin Xu <marvinxu99@hotmail.com>
2021-02-12 15:31:26 -0800

    flake8 style changes

--- itrac/apps.py
+++ itrac/apps.py
@@ -3,6 +3,9 @@ from django.apps import AppConfig
 
 class iTracConfig(AppConfig):
     name = 'itrac'
+    verbose_name = "Issue Tracker"
+
+    # App specific settings:
 
     def ready(self):
-        import itrac.signals
+        import itrac.signals     # noqa
