72c2434bf4ffe423724c2e5881891a39601198d6
marvinxu99 <marvinxu99@hotmail.com>
2021-07-25 08:17:22 -0700

    changed model name from codevalueset to codeset

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import (Item, ItemIdentifier, ItemBarcode, ItemPrice, ItemPriceHist,
-                    CodeValueSet, CodeValue)
+                    CodeSet, CodeValue)
 
 
 class ItemIdentifierInline(admin.TabularInline):
@@ -47,7 +47,7 @@ class ItemAdmin(admin.ModelAdmin):
         obj.save()
 
 
-@admin.register(CodeValueSet)
+@admin.register(CodeSet)
 class CodeSetValueAdmin(admin.ModelAdmin):
     list_display = ('code_set', 'display', 'description', 'definition', 'active_ind', 'cache_ind',
                 'change_access_ind', 'create_dt_tm', 'create_id', 'updt_cnt', 'updt_dt_tm', 'updt_id')
